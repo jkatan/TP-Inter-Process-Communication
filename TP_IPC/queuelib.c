12383361b75947a3add9975dc0c4bda68c6b3ebf
matiheimann <mheimann@itba.edu.ar>
2018-03-26 01:31:20 -0300

    Add files via upload

---
+++ TP_IPC/queuelib.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "queuelib.h"
+
+queue* createQueue(int maxSize)
+{
+	
+	queue* newQueue = (queue*) malloc(sizeof(queue));
+	newQueue->front = NULL;
+	newQueue->rear = NULL;
+	newQueue->maxSize = maxSize;
+	newQueue->actualSize = 0;
+
+	return newQueue;
+}
+
+int isEmptyTheQueue(queue* myQueue)
+{
+	return (myQueue->actualSize == 0) ? 1 : 0;
+}
+
+void enqueElement(queue* myQueue, char* fileToEnque){
+	
+	if(myQueue->actualSize != myQueue->maxSize)
+	{
+		node* nodeToAdd = (node*) malloc(sizeof(node));
+
+		nodeToAdd->fileName = fileToEnque;
+		nodeToAdd->nextNode = NULL;
+
+		if(isEmptyTheQueue(myQueue))
+		{
+			myQueue->front = nodeToAdd;
+			
+		}
+		else
+		{
+			myQueue->rear->nextNode = nodeToAdd;
+		}
+
+		myQueue->actualSize++;
+		myQueue->rear = nodeToAdd;
+	}
+	else
+	{
+		perror("The queue is full");
+	}
+	return;
+}
+
+char* dequeElement(queue* myQueue)
+{
+	if(isEmptyTheQueue(myQueue)){
+		perror("The queue is empty");
+		return NULL;
+	}
+
+	char* fileNameToPop = myQueue->front->fileName;
+	myQueue->front = myQueue->front->nextNode;
+	myQueue->actualSize--;
+
+	return fileNameToPop;
+}
---
+++ TP_IPC/queuelib.h
@@ -0,0 +1,29 @@
+#ifndef QUEUELIB_H
+#define QUEUELIB_H value
+
+
+typedef struct node node;
+
+typedef struct node{
+
+	char* fileName;
+	node* nextNode;
+
+}node;
+
+typedef struct queue{
+
+	node* front;
+	node* rear;
+	int maxSize;
+	int actualSize;
+
+}queue;
+
+queue* createQueue(int maxSize);
+void enqueElement(queue* myQueue, char* fileToEnque);
+char* dequeElement(queue* myQueue);
+int isEmptyTheQueue(queue* myQueue);
+
+#endif
+
